ea92ffb8d45f3027fe428191c0fa10128d8d1f7d
jamaludinsalam <jamaludinsalam11@gmail.com>
2019-12-08 14:30:50 +0700

    update our menu items to navigate with router

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import MenuItem from '../menu-item/menu-item.component';
 
 import './directory.styles.scss';
+import { link } from 'fs';
 
 class Directory extends React.Component {
     constructor() {
@@ -50,8 +51,8 @@ class Directory extends React.Component {
         return (
             <div className='directory-menu'>
                 {
-                    this.state.sections.map(({title, imageUrl, id, size}) => (
-                        <MenuItem key={id} title={title} imageUrl={imageUrl} size={size}/>
+                    this.state.sections.map(({id, ...otherSectionProps}) => (
+                        <MenuItem key={id} {...otherSectionProps}/>
                     ))
                 }
             </div>
